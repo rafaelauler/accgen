b06caa3b90646dee0984e4f23a28e0fcc06f3d27
Bruno Cardoso <brunocardoso@gmail.com>
2009-09-19 02:15:18 +0000

    Add group feature

--- InsnFormat.h
+++ InsnFormat.h
@@ -13,25 +13,29 @@ namespace backendgen {
     FormatField(const char *name, int sizeInBits, 
                 int startBitPos, int endBitPos) : 
       Name(name), SizeInBits(sizeInBits), StartBitPos(startBitPos), 
-      EndBitPos(endBitPos) {}
+      EndBitPos(endBitPos), GroupId(0) {}
 
     ~FormatField() {}
     const char *getName() const { return Name; }
     unsigned getSizeInBits() const { return SizeInBits; }
     unsigned getStartBitPos() const { return StartBitPos; }
     unsigned getEndBitPos() const { return EndBitPos; }
+    void incGroupId() { GroupId++; }
+    unsigned getGroupId() { return GroupId; }
   
   private:
     const char *Name;
     unsigned SizeInBits;
     unsigned StartBitPos;
     unsigned EndBitPos;
+    unsigned GroupId;
   };
   
   class InsnFormat {
   public:
     InsnFormat(const char *name, int sizeInBits) : 
-      Name(name), SizeInBits(sizeInBits), BaseLLVMFormatClassName("InstSP16") {}
+      Name(name), SizeInBits(sizeInBits), BaseLLVMFormatClassName("InstSP16"),
+      TmpLastField(-1), NumGroups(0) {}
 
     ~InsnFormat() {}
     const char *getName() const { return Name; }
@@ -39,6 +43,16 @@ namespace backendgen {
 
     void addField(FormatField *FF) {
       Fields.push_back(FF);
+
+      if (TmpLastField < 0)
+        TmpLastField = FF->getEndBitPos();
+
+      if (FF->getEndBitPos() > TmpLastField) {
+        FF->incGroupId();
+        NumGroups++;
+      }
+
+      TmpLastField = FF->getEndBitPos();
     }
 
     FormatField *getField(unsigned Num) {
@@ -58,12 +72,16 @@ namespace backendgen {
     std::string& getBaseLLVMFormatClassName() {
       return BaseLLVMFormatClassName;
     }
+
+    unsigned getNumGroups() const { return NumGroups; } 
   
   private:
     std::vector<FormatField *> Fields;
     const char *Name;
     unsigned SizeInBits;
     std::string BaseLLVMFormatClassName;
+    int TmpLastField;
+    unsigned NumGroups;
   };
 
 
