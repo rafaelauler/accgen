b96708e455cde515d370ce19fe9fb206238c3f88
Rafael Auler <rafael.auler@lsc.ic.unicamp.br>
2010-03-09 21:19:18 +0000

    Forgot new header

---
+++ Support.h
@@ -0,0 +1,56 @@
+//===- Support.h - Header file for support classes ---------*- C++ -*------===//
+//
+//------------------------------------------------------------------------------
+// Support classes and templates
+//------------------------------------------------------------------------------
+
+#include <list>
+
+namespace backendgen {
+
+template<class T, class OP, class Function>
+bool ApplyToLeafs(T* root, Function f) {
+  bool ReturnOk = true; 
+  std::list<T*> Queue;
+  Queue.push_back(root);
+  while (Queue.size() > 0) {
+    T* Element = Queue.front();
+    Queue.pop_front();
+    OP Op = dynamic_cast<OP>(*Element);
+    if (Op != NULL) {
+      for (int I = 0, E = Op->getArity(); I != E; ++I) {
+	Queue.push_back(&(*Op)[I]);
+      }
+      continue;
+    }
+
+    ReturnOk = ReturnOk && f(Element);
+  }
+
+  return ReturnOk;
+}
+
+template<class T, class OP, class Function>
+bool ApplyToAllNodes(T* root, Function f) {
+  bool ReturnOk = true; 
+  std::list<T*> Queue;
+  Queue.push_back(root);
+  while (Queue.size() > 0) {
+    T* Element = Queue.front();
+    Queue.pop_front();
+    OP Op = dynamic_cast<OP>(*Element);
+    if (Op != NULL) {
+      for (int I = 0, E = Op->getArity(); I != E; ++I) {
+	Queue.push_back(&(*Op)[I]);
+      }
+      ReturnOk = ReturnOk && f(Element);
+      continue;
+    }
+
+    ReturnOk = ReturnOk && f(Element);
+  }
+
+  return ReturnOk;
+}
+
+}
